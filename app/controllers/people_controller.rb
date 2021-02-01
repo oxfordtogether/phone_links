40d72169f7bb39d90a33575f46a874697590b535
VickyClose <vicky.r.close@gmail.com>
2021-02-01 20:17:19 +0000

    Controller/views for person

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,37 @@
+class PeopleController < ApplicationController
+  before_action :set_person, only: %i[edit update]
+
+  def new
+    @person = Person.new
+  end
+
+  def edit; end
+
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to @person, notice: "Person was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @person.update(person_params)
+      redirect_to @person, notice: "Person was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
+  def person_params
+    params.require(:person).permit(:title, :first_name, :last_name, :email, :phone)
+  end
+end
