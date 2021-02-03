ede78e2d4f69197918ab1cf973fa522626e5334e
VickyClose <vicky.r.close@gmail.com>
2021-02-03 16:52:22 +0000

    Add support page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "support", to: "pages#support"
 
   get "login", to: "login#show"
   get "invalid_permissions", to: "login#invalid_permissions"
