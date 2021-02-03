097841fe0f45b350fce1986129fccc5db8b8d55e
VickyClose <vicky.r.close@gmail.com>
2021-02-03 11:04:17 +0000

    Get js from ocl_tools working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,16 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 
+import ocl_tools from 'ocl_tools'
+
 const application = Application.start()
 const context = require.context('controllers', true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+
+const ocl_tools_controllers = Object.keys(ocl_tools.controllers).map((label) => (
+  {'identifier': label, 'controllerConstructor': ocl_tools.controllers[label] }
+))
+
+const context_list = definitionsFromContext(context).concat(ocl_tools_controllers)
+
+application.load(context_list)
 StimulusReflex.initialize(application, { consumer })
