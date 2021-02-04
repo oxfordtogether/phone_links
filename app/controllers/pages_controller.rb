5474078de9b794cf06a858dd2c746a0a6081f539
VickyClose <vicky.r.close@gmail.com>
2021-02-04 07:10:13 +0000

    Start on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+    @inbox_items = [
+      { summary: "Referral for Jane Johns", date: "10:45" },
+      { summary: "Caller application from Mark Matthews", date: "yesterday" },
+      { summary: "Safeguarding flag raised by Poppy Patal", date: "5 days ago" },
+      { summary: "Referral for Alisha Anderson", date: "8th Jan" },
+    ]
+  end
 
   def support; end
 
