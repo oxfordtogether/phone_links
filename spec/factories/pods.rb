de92152a0cbfea112dde4cf1f2a75421981740f4
VickyClose <vicky.r.close@gmail.com>
2021-02-05 19:00:33 +0000

    More UI stuff

--- spec/factories/pods.rb
+++ spec/factories/pods.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :pod do
-    name { FFaker::Color.name }
+    name { ("A".."Z").to_a[rand(26)] + ("A".."Z").to_a[rand(26)] }
     pod_leader factory: :pod_leader
     theme { ["Older people", "Under 30s", "Mental health", "East Oxford", "West Oxford", "Physical illness"].sample }
   end
