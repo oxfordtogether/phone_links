cbbd8915c7121de42f48687cb2940753ea497aaa
VickyClose <vicky.r.close@gmail.com>
2021-02-15 20:30:02 +0000

    Ensure factory pod name is actually unique

--- spec/factories/pods.rb
+++ spec/factories/pods.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :pod do
-    # might fail as this isn't guarenteed to be unique
-    name { ("A".."Z").to_a[rand(26)] + ("A".."Z").to_a[rand(26)] + ("A".."Z").to_a[rand(26)] }
+    sequence(:name) { |n| "#{('A'..'Z').to_a[rand(26)]}#{n}" }
     pod_leader { PodLeader.all.filter { |p| p.pod.nil? }.sample || create(:pod_leader) }
     theme { ["Older people", "Under 30s", "Mental health", "East Oxford", "West Oxford", "Physical illness"].sample }
   end
