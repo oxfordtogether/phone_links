885c8d1959a3124143bad0f6d0ee9725119603fa
VickyClose <vicky.r.close@gmail.com>
2021-02-15 20:37:37 +0000

    Update pod mismatch text

--- app/models/match.rb
+++ app/models/match.rb
@@ -15,14 +15,14 @@ class Match < ApplicationRecord
 
   def pod_mismatch
     # handle caller/callee not having a pod
-    return "Caller and Callee are not assigned to a pod. Go to their profile pages to assign them to pod: #{pod.name}." unless caller.pod.present? && callee.pod.present?
-    return "Caller is not assigned to a pod. Go to their profile page to assign them to pod: #{pod.name}." unless caller.pod.present?
-    return "Callee is not assigned to a pod. Go to their profile page to assign them to pod: #{pod.name}." unless callee.pod.present?
+    return "Caller and Callee are not assigned to a pod, match is assigned to #{pod.name}." unless caller.pod.present? && callee.pod.present?
+    return "Caller is not assigned to a pod, match is assigned to #{pod.name}." unless caller.pod.present?
+    return "Callee is not assigned to a pod, match is assigned to #{pod.name}." unless callee.pod.present?
 
     # handle caller/callee being part of a different pod
-    return "Caller is assigned to #{caller.pod.name}, callee is assigned to #{callee.pod.name}. Go to their profile page to reassign them to pod: #{pod.name}" unless caller.pod == pod && callee.pod == pod
-    return "Caller is assigned to #{caller.pod.name}. Go to their profile page to reassign them to pod: #{pod.name}" unless caller.pod == pod
-    return "Callee is assigned to #{callee.pod.name}. Go to their profile page to reassign them to pod: #{pod.name}" unless callee.pod == pod
+    return "Caller is assigned to #{caller.pod.name}, callee is assigned to #{callee.pod.name}, match is assigned to #{pod.name}." unless caller.pod == pod && callee.pod == pod
+    return "Caller is assigned to #{caller.pod.name}, match is assigned to #{pod.name}." unless caller.pod == pod
+    return "Callee is assigned to #{callee.pod.name}, match is assigned to #{pod.name}." unless callee.pod == pod
 
     false
   end
