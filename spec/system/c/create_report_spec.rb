7cece6c97658988ff10f15a4f757cc467dcdb8ad
Anthony Aimon <anthony.aimon@gmail.com>
2021-02-17 23:15:59 +0000

    attempt at testing report

---
+++ spec/system/c/create_report_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe "create report", type: :system do
+  let!(:callers) { create_list(:report, 10) }
+
+    Report.all
+  end
+
+  it "works" do
+    login_as nil
+
+    visit "/c"
+    click_on "Reports"
+    click_on "New"
+
+    select callers[7].names, from: "Match"
+    select "Great!", from: "How was the call?"
+    date_picker_fill_in "Call date", datetime: Datetime.parse("2020-01-01")
+    select "12", from: "report[datetime(4i)]"
+    select "05", from: "report[datetime(5i)]"
+    select "15 to 30 minutes", from "Call duration"
+    fill_in "Summary", with: "This is a test"
+
+    expect do
+      click_on "Submit report"
+    end.to change { Report.count }.by(1)
+
+    report = Report.last
+
+    expect(page).to have_current_path("/c/reports")
+
+    expect(report.summary).to eq("This is a test")
+
+  end
+
+  it "redirect back to correct page on cancel" do
+    login_as nil
+
+    visit "/c/reports/new"
+
+    click_on "Cancel"
+    expect(page).to have_current_path("/c")
+  end
+end
