97a26ecdb5ad569d99de300c8209dc8c8504fc6b
Anthony Aimon <anthony.aimon@gmail.com>
2021-02-18 06:56:19 +0000

    committing not working test

--- spec/system/c/create_report_spec.rb
+++ spec/system/c/create_report_spec.rb
@@ -1,24 +1,23 @@
 require "rails_helper"
 
 RSpec.describe "create report", type: :system do
-  let!(:callers) { create_list(:report, 10) }
-
-    Report.all
-  end
+  let!(:person) { create(:person) }
+  let!(:caller) { create(:caller) }
+  let!(:reports) { create_list(:report, 10) }
 
   it "works" do
-    login_as nil
+    # login_as nil
 
     visit "/c"
     click_on "Reports"
     click_on "New"
 
-    select callers[7].names, from: "Match"
+    select matches[7].names, from: "Match"
     select "Great!", from: "How was the call?"
     date_picker_fill_in "Call date", datetime: Datetime.parse("2020-01-01")
     select "12", from: "report[datetime(4i)]"
     select "05", from: "report[datetime(5i)]"
-    select "15 to 30 minutes", from "Call duration"
+    select "15 to 30 minutes", from: "Call duration"
     fill_in "Summary", with: "This is a test"
 
     expect do
@@ -30,7 +29,6 @@ RSpec.describe "create report", type: :system do
     expect(page).to have_current_path("/c/reports")
 
     expect(report.summary).to eq("This is a test")
-
   end
 
   it "redirect back to correct page on cancel" do
