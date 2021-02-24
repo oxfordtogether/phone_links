17845f5d4a4db31425b88f46990498d3b9d635a1
Anthony Aimon <anthony.aimon@gmail.com>
2021-02-24 16:01:08 +0000

    link not working but personalised caller name set up

--- app/controllers/c/pages_controller.rb
+++ app/controllers/c/pages_controller.rb
@@ -1,6 +1,8 @@
 class C::PagesController < C::CController
   def home
     @current_user = current_user
+    @current_caller = current_caller
+
     # @reports_count = Callee.with_matches.all.filter(&:waiting?).count
     matches = current_caller.match_ids
     @reports = Report.where(match_id: matches)
