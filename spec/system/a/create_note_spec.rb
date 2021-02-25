f6e14c5e6336fb834ca585001e6785ba7b5830c7
VickyClose <vicky.r.close@gmail.com>
2021-02-25 16:31:42 +0000

    Test create/edit/delete notes

---
+++ spec/system/a/create_note_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "create note", type: :system do
+  let!(:callee) { create(:callee) }
+  let!(:admin) { create(:admin, active: true) }
+
+  before do
+    ENV["BYPASS_AUTH"] = "false"
+  end
+
+  after do
+    ENV["BYPASS_AUTH"] = "true"
+  end
+
+  it "creates new note" do
+    login_as admin.person
+
+    visit "/a/people/#{callee.person.id}"
+    click_on "New note"
+    expect(page).to have_current_path("/a/people/#{callee.person.id}/notes/new")
+
+    fill_in "Content", with: "cool thing"
+
+    expect { click_on "Save" }.to change { Note.count }.by(1)
+
+    expect(page).to have_current_path("/a/people/#{callee.person.id}/events")
+
+    note = Note.last
+    expect(note.person).to eq(callee.person)
+    expect(note.content).to eq("cool thing")
+    expect(note.created_by).to eq(admin.person)
+
+    expect(Events::NoteChanged.count).to eq(1)
+  end
+end
---
+++ spec/system/a/delete_note_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "edit note", type: :system do
+  let!(:callee) { create(:callee) }
+  let!(:admin) { create(:admin, active: true) }
+  let!(:note) do
+    note = create(:note, person: callee.person, created_by: admin.person)
+    note.create_events!
+    note
+  end
+
+  before do
+    ENV["BYPASS_AUTH"] = "false"
+  end
+
+  after do
+    ENV["BYPASS_AUTH"] = "true"
+  end
+
+  it "creates new note" do
+    login_as admin.person
+
+    visit "/a/people/#{callee.person.id}"
+    expect(page).not_to have_content("Edit note")
+
+    note_edit_path = "/a/notes/#{note.id}/edit"
+    find(:xpath, "//a[@href='#{note_edit_path}']").click
+
+    expect(current_path).to eq("/a/notes/#{note.id}/edit")
+    expect(page).to have_content("Edit note")
+
+    expect { click_on "Delete" }.to change { Events::NoteChanged.count }.by(1)
+
+    expect(page).to have_current_path("/a/people/#{callee.person.id}/events")
+
+    note.reload
+    expect(note.deleted_at).not_to eq(nil)
+  end
+end
