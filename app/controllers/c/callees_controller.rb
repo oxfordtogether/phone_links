a4e7a11068fa2f3306977eec41d23b58a90024de
Anthony Aimon <anthony.aimon@gmail.com>
2021-03-01 09:57:03 +0000

    add max-width for homepage

---
+++ app/controllers/c/callees_controller.rb
@@ -0,0 +1,62 @@
+class C::ReportsController < C::CController
+  before_action :set_report, only: %i[show edit update destroy]
+
+  # GET /reports
+  def index
+    matches = current_caller.match_ids
+    @reports = Report.where(match_id: matches)
+  end
+
+  # GET /reports/1
+  def show; end
+
+  # GET /reports/new
+  def new
+    @report = Report.new
+    @redirect_on_cancel = params[:redirect_on_cancel] || c_path
+    matches = current_caller.match_ids
+    @reports = Report.where(match_id: matches)
+    @matches = current_caller.matches
+  end
+
+  # GET /reports/1/edit
+  def edit; end
+
+  # POST /reports
+  def create
+    @report = Report.new(report_params)
+
+    if @report.save
+      redirect_to c_reports_path, notice: "Report was successfully created."
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /reports/1
+  def update
+    if @report.update(report_params)
+      redirect_to @report, notice: "Report was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /reports/1
+  def destroy
+    @report.destroy
+    redirect_to reports_url, notice: "Report was successfully destroyed."
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def report_params
+    params.require(:report).permit(:match_id, :duration, :summary, :datetime, :callee_state)
+  end
+end
