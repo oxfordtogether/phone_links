306f9b92fbe0fafcaf518cfedd4cb38a43925e2b
VickyClose <vicky.r.close@gmail.com>
2021-03-02 15:12:55 +0000

    Nicer formatting for emergency contact form

--- app/javascript/controllers/emergency_contact_controller.js
+++ app/javascript/controllers/emergency_contact_controller.js
@@ -1,19 +1,17 @@
 import ApplicationController from "./application_controller";
 
 export default class extends ApplicationController {
-  static targets = ["newForm", "newButton", "hideButton"];
+  static targets = ["newForm", "newButton"];
 
   showForm(event) {
     event.preventDefault()
     this.newFormTarget.classList.remove("hidden");
     this.newButtonTarget.classList.add("hidden");
-    this.hideButtonTarget.classList.remove("hidden");
   }
 
   hideForm(event) {
     event.preventDefault()
     this.newFormTarget.classList.add("hidden");
     this.newButtonTarget.classList.remove("hidden");
-    this.hideButtonTarget.classList.add("hidden");
   }
 }
