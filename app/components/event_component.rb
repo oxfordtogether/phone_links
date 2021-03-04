fbc431666481d5596c6da4da5c6f4a3bdc2d603a
VickyClose <vicky.r.close@gmail.com>
2021-03-04 11:24:09 +0000

    Update report event element

--- app/components/event_component.rb
+++ app/components/event_component.rb
@@ -1,5 +1,5 @@
 class EventComponent < ViewComponent::Base
-  delegate :timeline_item, :format_date, to: :helpers
+  delegate :timeline_item, :format_date, :icon, to: :helpers
 
   def initialize(event:, last_event:)
     @event = event
