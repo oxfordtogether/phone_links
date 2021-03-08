62196aa3b21c8afd4bb0e7ca9b867f4a2c699515
VickyClose <vicky.r.close@gmail.com>
2021-03-08 10:01:39 +0000

    Add demo environment variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,17 @@ class ApplicationController < ActionController::Base
 
   default_form_builder OclTools::TailwindFormBuilder
 
+  before_action :demo
+
   def current_user
     if bypass_auth?
       Admin.first.person if Admin.count >= 1
     elsif session[:person_id]
       Person.find(session[:person_id])
     end
+  end
+
+  def demo
+    @demo ||= ENV["DEMO"]
   end
 end
