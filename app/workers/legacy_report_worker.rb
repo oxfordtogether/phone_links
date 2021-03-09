1ab0b0eda21d45a43b623d30fb853c0b65c59877
VickyClose <vicky.r.close@gmail.com>
2021-03-09 15:53:27 +0000

    Update legacy report importer

--- app/workers/legacy_report_worker.rb
+++ app/workers/legacy_report_worker.rb
@@ -14,5 +14,17 @@ class LegacyReportWorker
     # J
     syncer = LegacyReportSyncer.new(1, "1CHF4p6p1dJE5nyJ9geOJDuPgiSCdctFU_Bo9KxtE0pE", worksheet_index: 0, timestamp_format: "%d/%m/%Y %H:%M:%S")
     syncer.sync
+
+    # L
+    syncer = LegacyReportSyncer.new(8, "1w4Vnah0rXUkc8fLo9zOdBxRLdKlR8ePaIYPfqZdidNM", worksheet_index: 2, timestamp_format: "%d/%m/%Y %H:%M:%S")
+    syncer.sync
+
+    # K
+    syncer = LegacyReportSyncer.new(7, "1mTh90tQ1azqcPyRcseyavOhE2zql59bKgEiJkudzb8A", worksheet_index: 0, timestamp_format: "%d/%m/%Y %H:%M:%S")
+    syncer.sync
+
+    # F
+    syncer = LegacyReportSyncer.new(6, "1x7kLT-D7slLTP_EhbCMbRUQQw1uapIPDYxX0pLZcLdA", worksheet_index: 0, timestamp_format: "%d/%m/%Y %H:%M:%S")
+    syncer.sync
   end
 end
