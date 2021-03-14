a85d8566d8a7fb864e673f1d13630f918ff02c00
VickyClose <vicky.r.close@gmail.com>
2021-03-14 16:17:33 +0000

    Sort reports

--- app/controllers/c/matches_controller.rb
+++ app/controllers/c/matches_controller.rb
@@ -1,6 +1,6 @@
 class C::MatchesController < C::CController
   def show
     @match = @fetcher.match(params[:id])
-    @reports = @fetcher.reports_for_match(@match)
+    @reports = @fetcher.reports_for_match(@match).sort_by(&:date_of_call).reverse
   end
 end
