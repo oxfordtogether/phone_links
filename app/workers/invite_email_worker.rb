049fc47df56173b732c808d8133222673268eb45
VickyClose <vicky.r.close@gmail.com>
2021-03-24 10:42:54 +0000

    Pass params correctly

--- app/workers/invite_email_worker.rb
+++ app/workers/invite_email_worker.rb
@@ -4,6 +4,6 @@ class InviteEmailWorker
 
   def perform(id)
     person = Person.find(id)
-    InviteMailer.with(person).invitation_email.deliver
+    InviteMailer.with(person: person).invitation_email.deliver
   end
 end
