d0e7964eb8b794cc40b3d7ebd1973e5009e35d32
VickyClose <vicky.r.close@gmail.com>
2021-04-12 21:38:27 +0100

    Update report pdf

--- app/controllers/pl/reporting_controller.rb
+++ app/controllers/pl/reporting_controller.rb
@@ -2,7 +2,7 @@ class Pl::ReportingController < Pl::PlController
   def pod_reporting_pdf
     @pod = @fetcher.pod(params[:id])
 
-    @all_reports = Report.where(match_id: Pod.find(@pod.id).matches.map(&:id)).or(Report.where(legacy_pod_id: @pod.id))
+    @all_reports = Report.where(match_id: Pod.find(@pod.id).matches.map(&:id))
     @reports = @all_reports.where("created_at >= ?", "2020-01-01")
 
     @reports_by_month = @reports.group_by_month(:created_at).count
