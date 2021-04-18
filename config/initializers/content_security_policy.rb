09ace02d451e971c6b27ad32d6d7949716aa7ce1
VickyClose <vicky.r.close@gmail.com>
2021-04-18 20:07:30 +0100

    Disable content-security-policy header

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,26 +4,26 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src :none
+# Rails.application.config.content_security_policy do |policy|
+#   policy.default_src :none
 
-  policy.font_src    :none
-  policy.img_src     :self, "http://www.w3.org/2000/svg"
-  policy.object_src  :none
+#   policy.font_src    :none
+#   policy.img_src     :self, "http://www.w3.org/2000/svg"
+#   policy.object_src  :none
 
-  policy.script_src  :self
-  policy.style_src   :self, :unsafe_inline
+#   policy.script_src  :self
+#   policy.style_src   :self, :unsafe_inline
 
-  # If you are using webpack-dev-server then specify webpack-dev-server host
-  policy.connect_src :self,
-                      :https,
-                      "http://localhost:3035",
-                      "ws://localhost:3035",
-                      "ws://127.0.0.1:53360/cable" if Rails.env.development? || Rails.env.test?
+#   # If you are using webpack-dev-server then specify webpack-dev-server host
+#   policy.connect_src :self,
+#                       :https,
+#                       "http://localhost:3035",
+#                       "ws://localhost:3035",
+#                       "ws://127.0.0.1:53360/cable" if Rails.env.development? || Rails.env.test?
 
-  # Specify URI for violation reports
-  # policy.report_uri "/csp-violation-report-endpoint"
-end
+#   # Specify URI for violation reports
+#   # policy.report_uri "/csp-violation-report-endpoint"
+# end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
